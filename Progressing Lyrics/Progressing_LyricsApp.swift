a86c315674ad5c07676f5ed4cc8f40920068ad19
Magesh Sridhar <mags2smart@gmail.com>
2024-12-28 20:04:04 -0700

    Initial Commit

---
+++ Progressing Lyrics/Progressing_LyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progressing_LyricsApp.swift
+//  Progressing Lyrics
+//
+//  Created by Magesh Sridhar on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Progressing_LyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
